74e62299686179f02d78489f4c6db931e92dea3b
Vito Mule <mulevito@gmail.com>
2016-01-13 21:52:25 +0000

    use opendir/readdir

--- plog.c
+++ plog.c
@@ -1,22 +1,22 @@
-// Copyright 2015 Vito Mule'
-//
-// This file may be used subject to the terms and conditions of the
-// GNU Library General Public License Version 2 as published by the
-// Free Software Foundation.This program is distributed in the hope
-// that it will be useful, but WITHOUT ANY WARRANTY; without even the
-// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-// PURPOSE. See the GNU Library General Public License for more
-// details.
+/*Copyright 2015 Vito Mule'
 
+This file may be used subject to the terms and conditions of the
+GNU Library General Public License Version 2 as published by the
+Free Software Foundation.This program is distributed in the hope
+that it will be useful, but WITHOUT ANY WARRANTY; without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU Library General Public License for more
+details.*/
+
+#include <dirent.h>
+#include <errno.h>
+#include <limits.h>
+#include <regex.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <regex.h>
-#include <limits.h>
 #include <unistd.h>
-#include <errno.h>
-#include <dirent.h>
 
 int main(int argc, char const *argv[]) {
   float version = 1.0;
@@ -29,14 +29,14 @@ int main(int argc, char const *argv[]) {
     exit(255);
   }
 
-  // Allowed on the command line:
-  // --version
-  // -V
-  // /proc/nnnn
-  // nnnn
-  // where nnnn is any number that doesn't begin with 0.
-  // If --version or -V are present, further arguments are ignored
-  // completely.
+  /*Allowed on the command line:
+    --version
+    -V
+    /proc/nnnn
+    nnnn
+    where nnnn is any number that doesn't begin with 0.
+    If --version or -V are present, further arguments are ignored
+    completely.*/
 
   regcomp(&re_pid, "^((/proc/+)?[1-9][0-9]*|-V|--version)$",
           REG_EXTENDED|REG_NOSUB);
@@ -56,15 +56,15 @@ int main(int argc, char const *argv[]) {
 
   regcomp(&re_log, "^(.*log)$",REG_EXTENDED|REG_NOSUB);
 
-  // At this point, all arguments are in the form /proc/nnnn
-  // or nnnn, so a simple check based on the first char is
-  // possible.
+  /*At this point, all arguments are in the form /proc/nnnn
+    or nnnn, so a simple check based on the first char is
+    possible.*/
 
-  int entries;
-  struct dirent **namelist;
+  struct dirent *namelist;
   char* fullpath = (char*) malloc(PATH_MAX+1);
   char* linkpath = (char*) malloc(PATH_MAX+1);
   char buf[PATH_MAX+1];
+  DIR *proc_dir;
 
   if (argv[1][0] != '/') {
     strcpy(fullpath, "/proc/");
@@ -77,26 +77,22 @@ int main(int argc, char const *argv[]) {
   }
 
   printf("Pid no %s:\n", argv[1]);
-  entries = scandir(fullpath, &namelist, NULL, NULL);
-  if (entries < 1) {
-    perror("scandir");
+  proc_dir = opendir(fullpath);
+  if (!proc_dir) {
+    perror("opendir PID dir: ");
   }
-  else {;
-    for (i = 0; i < entries; i++) {
-      strcpy(linkpath, fullpath);
-      strcat(linkpath, namelist[i]->d_name);
-      readlink(linkpath, buf, PATH_MAX -1);
+  while((namelist = readdir(proc_dir))) {
+    strcpy(linkpath, fullpath);
+    strcat(linkpath, namelist->d_name);
+    readlink(linkpath, buf, PATH_MAX -1);
 
-      if (regexec(&re_log, buf, 0, NULL, 0) == 0) {
-        printf("Log path: %s\n", buf);
-      }
-      free(namelist[i]);
-      memset(&linkpath[0], 0, sizeof(linkpath));
-      memset(&buf[0], 0, sizeof(buf));
+    if (regexec(&re_log, buf, 0, NULL, 0) == 0) {
+      printf("Log path: %s\n", buf);
     }
-  free(namelist);
+    memset(&linkpath[0], 0, sizeof(linkpath));
+    memset(&buf[0], 0, sizeof(buf));
   }
   memset(&fullpath[0], 0, sizeof(fullpath));
-
+  regfree(&re_log);
   return 0;
 }
